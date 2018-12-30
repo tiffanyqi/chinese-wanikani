2a2c987547e2687e063904493210b7b7a9a6907d
Tiffany Qi <tiffany19qi@gmail.com>
2018-12-30 15:37:22 -0800

    some clean up and validation good

--- script.js
+++ script.js
@@ -1,43 +1,92 @@
-let character;
-let type;
-
 $(document).ready(function() {
-  const url = 'http://ccdb.hemiola.com/characters/?filter=gb&fields=kDefinition,kMandarin,string';
-  $.getJSON(url, function(data) {
-    character = getRandomCharacter(data);
-    type = getType();
-  });
-
+  loadRandomCharacter();
 });
 
-function getRandomCharacter(data) {
-  const max = data.length;
-  const randomNumber = getRandomNumber(max);
-  const character = data[randomNumber];
-  $('#character').text(function() {
-    return character.string;
-  });
-}
+/* User input functions */
 
-function getType() {
-  const randomNumber = getRandomNumber(2);
-  const type = randomNumber === 0 ? 'definition' : 'pinyin';
-  $('#type').text(function() {
-    return `${type}:`;
+function loadRandomCharacter() {
+  const url = 'http://ccdb.hemiola.com/characters/?filter=gb&fields=kDefinition,kMandarin,string';
+  $.getJSON(url, function(data) {
+    window.character = getRandomCharacter(data);
+    $('#character').text(function() {
+      return window.character.string;
+    });
+    window.type = getType();
+    $('#type').text(function() {
+      return `${window.type}:`;
+    });
   });
+  clearFields();
+  return false;
 }
 
 function validate() {
   const {character, type} = window;
   const userInput = $('#userInput').val();
-  const key = type === 'definition' ? 'kDefinition' : 'kMandarin';
-  const results = userInput === character[type] ? `you're right!` : `you're not right`;
+  const results = isUserCorrect(userInput, type, character) ? `you're right!` : `you're not right`;
   $('#results').text(function() {
     return results;
   });
   return false;
 }
 
+function displayAnswer() {
+  const {character, type} = window;
+  const key = getKey(type);
+  $('#answer').text(function() {
+    return character[key];
+  });
+  return false;
+}
+
+/* Helper functions */
+
 function getRandomNumber(max) {
   return Math.floor(Math.random() * max);
+}
+
+function getRandomCharacter(data) {
+  const max = data.length;
+  const randomNumber = getRandomNumber(max);
+  const character = data[randomNumber];
+  return character;
+}
+
+function getType() {
+  const randomNumber = getRandomNumber(2);
+  const type = randomNumber === 0 ? 'definition' : 'pinyin';
+  return type;
+}
+
+function getKey(type) {
+  return isDefinition(type) ? 'kDefinition' : 'kMandarin';
+}
+
+function isDefinition(type) {
+  return type === 'definition';
+}
+
+function isUserCorrect(userInput, type, character) {
+  const key = getKey(type);
+  const possibleCorrectAnswers = isDefinition(type) ? character[key].split(/[,;]\s+/) : character[key].split(' ');
+  let result = false;
+  if (possibleCorrectAnswers.includes(userInput)) {
+    result = true;
+  } else if (possibleCorrectAnswers.includes(userInput.toUpperCase())) {
+    result = true;
+  }
+  return result;
+}
+
+function clearFields() {
+  $('#answer').text(function() {
+    return '';
+  });
+  $('#character').text(function() {
+    return '';
+  });
+  $('#results').text(function() {
+    return '';
+  });
+  document.getElementById('userInput').value = '';
 }
