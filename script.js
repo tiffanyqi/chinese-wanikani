9adc9ac79ea5fd6797db5952226b9c175575c833
Tiffany Qi <tiffany19qi@gmail.com>
2018-12-30 14:32:51 -0800

    user interaction with input

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
+let character;
+let type;
+
 $(document).ready(function() {
-  $.getJSON('http://ccdb.hemiola.com/characters/?filter=gb&fields=kDefinition,kMandarin,string', function(data) {
-    getRandomCharacter(data);
+  const url = 'http://ccdb.hemiola.com/characters/?filter=gb&fields=kDefinition,kMandarin,string';
+  $.getJSON(url, function(data) {
+    character = getRandomCharacter(data);
+    type = getType();
   });
 
 });
 
 function getRandomCharacter(data) {
   const max = data.length;
-  const randomNumber = Math.floor(Math.random() * (max));
+  const randomNumber = getRandomNumber(max);
   const character = data[randomNumber];
   $('#character').text(function() {
     return character.string;
   });
+}
+
+function getType() {
+  const randomNumber = getRandomNumber(2);
+  const type = randomNumber === 0 ? 'definition' : 'pinyin';
+  $('#type').text(function() {
+    return `${type}:`;
+  });
+}
+
+function validate() {
+  const {character, type} = window;
+  const userInput = $('#userInput').val();
+  const key = type === 'definition' ? 'kDefinition' : 'kMandarin';
+  const results = userInput === character[type] ? `you're right!` : `you're not right`;
+  $('#results').text(function() {
+    return results;
+  });
+  return false;
+}
+
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max);
 }
