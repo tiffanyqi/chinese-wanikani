e451226a3b43551950cfb5824d76371b06af520f
Tiffany Qi <tiffany19qi@gmail.com>
2019-01-06 14:41:52 -0800

    character url works

--- wanikani/urls.py
+++ wanikani/urls.py
@@ -22,4 +22,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('<str:character>/', views.character, name='character'),
 ]
--- wanikani/views.py
+++ wanikani/views.py
@@ -1,4 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
 
 def index(request):
     return render(request, 'wanikani/index.html')
+
+def character(request, character):
+    context = {
+        'character': character,
+    }
+    return render(request, 'wanikani/character.html', context)
