e597dd4817762910bf479f8aebd82b51c41b8c60
Tiffany Qi <tiffany19qi@gmail.com>
2019-04-16 22:55:39 +0800

    remove uglify

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,12 +1,11 @@
 const webpack = require('webpack');
 const BundleTracker = require('webpack-bundle-tracker');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const config = require('./webpack.base.config.js');
 
 config.mode = 'production';
 
-config.output.path = require('path').resolve('./wanikani/static/dist');
+config.output.path = require('path').resolve('./wanikani/static/dist/');
 
 config.plugins = config.plugins.concat([
   new BundleTracker({filename: './webpack-stats-prod.json'}),
@@ -17,21 +16,5 @@ config.plugins = config.plugins.concat([
       'NODE_ENV': JSON.stringify('production')
   }}),
 ]);
-
-config.optimization = {
-  minimizer: [
-    // we specify a custom UglifyJsPlugin here to get source maps in production
-    new UglifyJsPlugin({
-      cache: true,
-      parallel: true,
-      uglifyOptions: {
-        compress: false,
-        ecma: 6,
-        mangle: true
-      },
-      sourceMap: true
-    })
-  ]
-};
 
 module.exports = config;
